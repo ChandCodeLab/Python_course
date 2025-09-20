3a8983738760f35aa6e4104e2f8db0a37b84fc4c
ChandCodeLab <manojchand3212@gmail.com>
2025-09-20 18:41:01 +0545

    variable in python

---
+++ Variables.py
@@ -0,0 +1,18 @@
+price = 10
+# update the value
+price =20    
+rating = 4.5
+name = "Manoj"
+is_published = True
+print(price)
+
+# Patinet detalis
+# We check in a patient named Jhon Smith.He's 20 years old and is a new patient
+
+patient_name ="Jhon Smith"
+age = 23
+is_new = True
+ 
+print(patient_name )
+print(age)
+print(is_new)
