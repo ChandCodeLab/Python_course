8e755e749108ec2c111d78729689a2dc8cd3d039
ChandCodeLab <manojchand3212@gmail.com>
2025-09-22 18:54:33 +0545

    string methods

---
+++ stringMethods.py
@@ -0,0 +1,21 @@
+# general purpose function - pritn and len
+course = 'python for beginners'
+print(len(course))
+
+# lowercase
+print(course.upper())
+
+#lowercase
+print(course.lower())
+
+#find- know the index of the particular character
+print(course.find('t'))
+
+# replace
+print(course.replace('beginners','Advanced'))
+print(course.replace('p','J'))
+
+# in operators -to check the word is in the variable or not 
+print("python" in course)  #ture
+print("Python" in course)  #false
+
