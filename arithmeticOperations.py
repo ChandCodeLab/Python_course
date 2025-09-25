b468f3872d437a3c67e05f908425b2b92997741e
ChandCodeLab <manojchand3212@gmail.com>
2025-09-25 22:34:09 +0545

    arithmetic Operations in python

---
+++ arithmeticOperations.py
@@ -0,0 +1,38 @@
+a=20
+b=10
+
+# Addition
+print(a+b )
+
+# Subtraction
+print(a-b)
+
+# Division
+print(a/b) #float value
+print(a//b) #int value
+
+# Modulus
+print(a%b)
+
+# Multiplication
+print(a*b)
+
+# Exponentional or power
+print(2**2)
+
+# add value to existing value
+c = 10
+c = c +3
+print(c)
+
+# augmented assignment operator
+
+x=10
+x-=3
+print('The value of first x is ',x)
+
+x+=3
+print('The value of second x is ',x)
+
+ 
+
