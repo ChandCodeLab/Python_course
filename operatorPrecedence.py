a7ec2779e2c48cc8ebf2c28aa7da80a394b49810
ChandCodeLab <manojchand3212@gmail.com>
2025-09-30 16:07:31 +0545

    operators precedence in python

---
+++ operatorPrecedence.py
@@ -0,0 +1,15 @@
+x = 10 + 2*3
+print(x)
+
+# parenthesis
+# exponentiation
+# multiplication or division
+# addition or subtraction 
+# left to right on the same precedence 
+
+b = 10 + 4 * 4**4
+print(b)
+
+# practice exercise 
+d = (2+3) * 10 -3
+print(d)
