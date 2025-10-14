930de5e65bf3d89cd2b7843205ffa09b5ddf3651
ChandCodeLab <manojchand3212@gmail.com>
2025-10-15 00:57:09 +0545

    tuples in python

---
+++ tuples_18.py
@@ -0,0 +1,39 @@
+# Create a tuple
+thistuple = ('apple', 'banana','cherry')
+print(thistuple)
+
+# Tuples allow duplicate values
+tuples =("manoj",'Ram',"Ram","Kalu")
+print(tuples)
+
+# Tuple length 
+print("The length of tuple is :",len(tuples))
+
+# Create Tuple With One Item
+#  -One item tuple, remember the comma
+tup =("apple",)
+print(tup)
+print(type(tup))
+
+# not a tuple /it is a string
+t=("APPLE")
+print(t)
+print(type(t))
+
+# Tuple Items - Data Types
+'''
+    -string 
+    int 
+    boolean
+    
+'''
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+
+# A tuple can contain different data types:
+T= (29,3,22,34,'Manoj' ,30.33,[2,4,44,53])
+print(T[6])
+
+# type()
+print(type(T))
